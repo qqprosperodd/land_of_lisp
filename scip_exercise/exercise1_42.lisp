5f52aaaa219a7320868c43d53200172ab92bba89
qqprosperodd <54343978+qqprosperodd@users.noreply.github.com>
2020-04-06 17:26:41 +0900

    Add files via upload

---
+++ scip_exercise/exercise1_42.lisp
@@ -0,0 +1,6 @@
+; Exercise 1.42: Let f and д be two one-argument functions.
+; The composition f after д is defined to be the function x -> f (д(x)).
+; Define a procedure compose that implements composition.
+; For example, if inc is a procedure that adds 1 to its argument,
+((compose square inc) 6)
+49
