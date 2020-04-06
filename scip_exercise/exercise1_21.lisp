a9023094a7255421784d2750c0269719f856f352
qqprosperodd <54343978+qqprosperodd@users.noreply.github.com>
2020-04-06 17:28:00 +0900

    Add files via upload

---
+++ scip_exercise/exercise1_21.lisp
@@ -0,0 +1,9 @@
+(define (smallest-divisor n) (find-divisor n 2))
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+    ((divides? test-divisor n) test-divisor)
+    (else (find-divisor n (+ test-divisor 1)))))
+(define (divides? a b) (= (remainder b a) 0))
+
+; Exercise 1.21: Use the smallest-divisor procedure to find
+; the smallest divisor of each of the following numbers: 199, 1999, 19999
