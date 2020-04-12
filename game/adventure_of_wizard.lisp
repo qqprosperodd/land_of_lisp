b17785ab4d969fc35d185f469b7f0a09e52258fb
qqprosperodd <54343978+qqprosperodd@users.noreply.github.com>
2020-04-12 23:06:58 +0900

    Add files via upload
    bug fixed

--- game/adventure_of_wizard.lisp
+++ game/adventure_of_wizard.lisp
@@ -5,44 +5,55 @@
                         (attic (you are in the attic.
                           there is a giant welding torch in the corner.))))
 ;
+
 (defun describe-location (location nodes)
   (cadr (assoc location nodes)))
 ;
-(defparameter *edegs* '((living-room (garden west door)
+
+(defparameter *edges* '((living-room (garden west door)
                                     (attic upstairs ladder))
                       (garden (living-room east door))
                       (attic (living-room downstairs ladder))))
 ;
+
 (defun describe-path (edge)
   `(there is a ,(caddr edge) going ,(cadr edge) from here.))
 ;
+
 (defun describe-paths (location edges)
   (apply #'append (mapcar #'describe-path (cdr (assoc location edges)))))
 ;
+
 (defparameter *objects* '(whiskey bucket frog chain))
 ;
+
 (defparameter *object-locations* '((whiskey living-room)
                                   (bucket living-room)
                                   (chain garden)
                                   (frog garden)))
 ;
+
 (defun objects-at (loc objs obj-locs)
   (labels ((at-loc-p (obj)
             (eq (cadr (assoc obj obj-locs)) loc)))
     (remove-if-not #'at-loc-p objs)))
 ;
+
 (defun describe-objects (loc objs obj-loc)
   (labels ((describe-obj (obj)
               `(you see a ,obj n the floor.)))
-      (apply #'append (mapcar #'describe-obj (obects-at loc objs obj-loc)))))
+      (apply #'append (mapcar #'describe-obj (objects-at loc objs obj-loc)))))
 ;
+
 (defparameter *location* 'living-room)
 ;
+
 (defun look ()
   (append (describe-location *location* *nodes*)
           (describe-paths *location* *edges*)
           (describe-objects *location* *objects* *object-locations*)))
 ;
+
 (defun walk (direction)
   (let ((next (find direction
                   (cdr (assoc *location* *edges*))
@@ -52,6 +63,7 @@
                 (look))
         '(you cannot go that way.))))
 ;
+
 (defun pickup (object)
   (cond ((member object
                   (objects-at *location* *objects* *object-locations*))
@@ -59,5 +71,6 @@
         `(you are now carrying the ,object))
         (t '(you cannot get that.))))
 ;
+
 (defun inventory ()
-  (cons 'items- (objects-at 'body *objects* *object-locations*)))  
+  (cons 'items- (objects-at 'body *objects* *object-locations*)))
